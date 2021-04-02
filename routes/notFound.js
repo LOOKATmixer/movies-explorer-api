3b65cc7badfce60d3091100d6e1ef191c7adf55d
LOOKATmixer <65297330+LOOKATmixer@users.noreply.github.com>
2021-04-02 18:13:34 +0500

    fix

--- routes/notFound.js
+++ routes/notFound.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const NotFoundError = require('../errors/NotFoundError');
 
-router.all('*', () => {
+router.use(() => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
 });
 
